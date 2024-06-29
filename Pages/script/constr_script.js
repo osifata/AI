bbabe54f53aa728a5e679f3a873eca165628c9d4
Libeausta <133878238+Libeausta@users.noreply.github.com>
2024-06-29 22:48:09 +0800

    Поля стали красивее

--- Pages/script/constr_script.js
+++ Pages/script/constr_script.js
@@ -164,19 +164,55 @@ chg_button.addEventListener('click', function() {
 	}
 });
 
-const fileUploader = document.querySelectorAll(".image-upload");
-const reader = new FileReader();
-const imageGrid = document.querySelectorAll(".preview");
 
-fileUploader.addEventListener('change', (event) => {
-const files = event.target.files;
-const file = files[0];
-reader.readAsDataURL(file);
-		
-reader.addEventListener('load', (event) => {
-	const img = document.createElement('img');
-	imageGrid.appendChild(img);
-	img.src = event.target.result;
-	img.alt = file.name;
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+		$('.image-upload-wrap').hide();
+		$('.file-upload-image').attr('src', e.target.result);
+		$('.file-upload-content').show();
+		$('.image-title').html(input.files[0].name);
+		};
+		reader.readAsDataURL(input.files[0]);
+	} else {
+		removeUpload();
+	}
+	}
+	function removeUpload() {
+	$('.file-upload-input').replaceWith($('.file-upload-input').clone());
+	$('.file-upload-content').hide();
+	$('.image-upload-wrap').show();
+	}
+	$('.image-upload-wrap').bind('dragover', function () {
+			$('.image-upload-wrap').addClass('image-dropping');
+		});
+		$('.image-upload-wrap').bind('dragleave', function () {
+			$('.image-upload-wrap').removeClass('image-dropping');
 });
+
+function readURL2(input2) {
+	if (input2.files && input2.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+		$('.image-upload-wrap2').hide();
+		$('.file-upload-image2').attr('src', e.target.result);
+		$('.file-upload-content2').show();
+		$('.image-title2').html(input2.files[0].name);
+		};
+		reader.readAsDataURL(input2.files[0]);
+	} else {
+		removeUpload2();
+	}
+	}
+	function removeUpload2() {
+	$('.file-upload-input2').replaceWith($('.file-upload-input2').clone());
+	$('.file-upload-content2').hide();
+	$('.image-upload-wrap2').show();
+	}
+	$('.image-upload-wrap2').bind('dragover', function () {
+			$('.image-upload-wrap2').addClass('image-dropping2');
+		});
+		$('.image-upload-wrap2').bind('dragleave', function () {
+			$('.image-upload-wrap2').removeClass('image-dropping2');
 });
